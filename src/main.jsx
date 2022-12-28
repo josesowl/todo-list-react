d3e4882ae6cb9d333aa6b78a34733fdc04fc5ab6
josesowl <jose.montesm@autonoma.edu.co>
2022-12-28 08:50:48 -0500

    in this commit was created all the routes with React route v6 to connect all the pages created

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {  BrowserRouter } from "react-router-dom";
 import './index.css'
 
-
-import { TodoApp } from './components/TodoApp';
+import { MainApp } from './useContext/MainApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <TodoApp />
-  </React.StrictMode>,
+  <BrowserRouter>
+    {/* <React.StrictMode> */}
+      <MainApp />
+    {/* </React.StrictMode> */}
+  </BrowserRouter>,
 )
